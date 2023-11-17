e77f4b60bc06e9f7dd6c6a86fd0dd524d31519d7
jun447 <junaidmumtaz16@gmail.com>
2023-11-17 16:23:40 +0500

    handled props

--- tailwind_prop/src/App.jsx
+++ tailwind_prop/src/App.jsx
@@ -1,33 +1,22 @@
 import './App.css'
+import Card from "./Components/Card.jsx";
 
 function App() {
-
+    let obj = {
+        username: `Junaid`,
+        age: 20,
+    }
+    let arr = [1, 2, 3, 4, 5]
 
   return (
     <>
         <h1 className="bg-orange-700 rounded-2xl mb-2">
             Hello world!
         </h1>
-        <div className="w-[300px] rounded-md border">
-            <img
-                src="https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
-                alt="Laptop"
-                className="h-[200px] w-full rounded-md object-cover"
-            />
-            <div className="p-4">
-                <h1 className="text-lg font-semibold">About Macbook</h1>
-                <p className="mt-3 text-sm text-gray-600">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-                    debitis?
-                </p>
-                <button
-                    type="button"
-                    className="mt-4 rounded-sm bg-black px-2.5 py-1 text-[10px] font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-                >
-                    Read
-                </button>
-            </div>
-        </div>
+        <Card myName={`Junaid`} btntxt={'Hai Bro'} data={obj} arry={arr}/>
+        <Card myName={`Ghori`}
+              // btntxt={"Explore"}
+        />
 
     </>
   )
