1219fdeeca6beed0b1bf66da3d1dce2dd26c0e4b
jun447 <junaidmumtaz16@gmail.com>
2023-11-22 19:22:43 +0500

    Added business Logic memoization and Deleted Unnecessary files

--- pswrdGenerator/src/App.jsx
+++ pswrdGenerator/src/App.jsx
@@ -1,14 +1,96 @@
-import { useState } from 'react'
-import './App.css'
+import {useCallback, useEffect, useState} from 'react'
 
 function App() {
-  const [length, setlength] = useState(8)
+    const [length, setLength] = useState(8);
+    const [isNumber, setIsNumber] = useState(false);
+    const [isSymbol, setIsSymbol] = useState(false);
+    const [password, setPassword] = useState('');
 
-  return (
-    <>
-        <h1 className={'text-4xl text-blue-400'} >React + Vite</h1>
-    </>
-  )
+    const passwrdGenerator = useCallback(() => {
+        let chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        let number = '0123456789';
+        let symbol = '!@#$%^&*()_+';
+        let passwrd = '';
+        if (isSymbol) {
+            chars += symbol;
+        }
+        if (isNumber) {
+            chars += number;
+        }
+
+        for (let i = 0; i < length; i++) {
+            let randomChar = Math.floor(Math.random() * chars.length+1); // gives random index
+            passwrd += chars.charAt(randomChar);
+        }
+        setPassword(passwrd);
+    }, [length, isNumber, isSymbol, setPassword]);
+
+    useEffect(() => {
+        passwrdGenerator();
+    }, [length,isNumber,isSymbol,passwrdGenerator]);
+
+    return (
+        <>
+
+            <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8
+                        bg-gradient-to-r from-orange-300 to-green-100 text-orange-500"
+            >
+                <h1 className='text-pink-400 font-bold text-center my-3'>Password generator</h1>
+                <div className="flex shadow rounded-lg overflow-hidden mb-4">
+                    <input
+                        type="text"
+                        value={password}
+                        className="outline-none w-full py-1 px-3"
+                        placeholder="Password"
+                        readOnly={true}
+                        // ref={passwordRef}
+                    />
+                    <button
+                        // onClick={copyPasswordToClipboard}
+                        className='outline-none bg-gradient-to-r to-orange-500 from-green-500
+                     text-white px-3 py-0.5 shrink-0'
+                    >copy
+                    </button>
+
+                </div>
+                <div className='flex text-sm gap-x-2'>
+                    <div className='flex items-center gap-x-1'>
+                        <input
+                            type="range"
+                            min={6}
+                            max={20}
+                            value={length}
+                            className='cursor-pointer'
+                            onChange={(e) => setLength(e.target.value)}
+                        />
+                        <label>Length: {length}</label>
+                    </div>
+                    <div className="flex items-center gap-x-1">
+                        <input
+                            type="checkbox"
+                            defaultChecked={isNumber}
+                            id="numberInput"
+                            onChange={() => {
+                                setIsNumber((prev) => !prev);
+                            }}
+                        />
+                        <label htmlFor="numberInput">Numbers</label>
+                    </div>
+                    <div className="flex items-center gap-x-1">
+                        <input
+                            type="checkbox"
+                            defaultChecked={isSymbol}
+                            id="characterInput"
+                            onChange={() => {
+                                setIsSymbol((prev) => !prev)
+                            }}
+                        />
+                        <label htmlFor="characterInput">Characters</label>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
