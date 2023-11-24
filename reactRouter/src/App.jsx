4b7ed006d40515ad71c6bf4ee15aa6504e61e497
jun447 <junaidmumtaz16@gmail.com>
2023-11-24 16:09:11 +0500

    tailwind configured

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+        <h1 className={'text-pink-400'} >React Router</h1>
+    </>
+  )
+}
+
+export default App
