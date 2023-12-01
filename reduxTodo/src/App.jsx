1fb2ac4e6bda16dc33d84ca645aa66faa36f046b
jun447 <junaidmumtaz16@gmail.com>
2023-12-01 22:27:02 +0500

    configured tailwind and created input fields

--- reduxTodo/src/App.jsx
+++ reduxTodo/src/App.jsx
@@ -1,13 +1,17 @@
 // import { useState } from 'react'
 import './App.css'
+import AddTodo from "./components/AddTodo.jsx";
+import Todo from "./components/Todo.jsx";
 
 function App() {
 
-  return (
-    <>
-        <h1>React + Vite + Redux</h1>
-    </>
-  )
+    return (
+        <>
+            <h1>Learn about redux toolkit</h1>
+            <AddTodo />
+            {/*<Todo />*/}
+        </>
+    )
 }
 
 export default App
