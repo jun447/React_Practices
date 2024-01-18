be74ce1fc2d30040b3eaf138f77455b80d8ad0a5
jun447 <junaidmumtaz16@gmail.com>
2024-01-18 12:34:38 +0500

    configured redux

--- capsotoneProject/src/App.jsx
+++ capsotoneProject/src/App.jsx
@@ -1,13 +1,48 @@
 import './App.css'
-
+import {useEffect, useState} from "react";
+import {useDispatch} from "react-redux";
+import authService from "./appwrite/auth.js";
+import {login,logout} from "./store/authSlice.js";
+import {Footer, Header} from "./components/index.js";
+import {Outlet} from "react-router-dom";
 
 function App() {
-    console.log(import.meta.env.VITE_APPWRITE_ENDPOINT)
-  return (
-    <>
-     <h1 className={'text-orange-400'} >HAI</h1>
-    </>
-  )
+    // console.log(import.meta.env.VITE_APPWRITE_ENDPOINT)
+    const [loading, setLoading] = useState(true)
+    const dispatch = useDispatch()
+
+  //   asking useEffect to check login or not
+    useEffect(() => {
+        authService.getCurrentUser()
+            .then((userData) => {
+                if (userData) {
+                    dispatch(login(userData))
+                }
+                else {
+                    dispatch(logout())
+                }
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+            .finally(() => {
+                setLoading(false)
+            })
+    }, []);
+
+    return !loading ? (
+        <div className={'min-h-screen flex flex-wrap content-between bg-gray-400'}>
+            <div className={'w-full block'}>
+                <Header/>
+                <main>
+                {/*todo*/}
+                    {/*<Outlet/>*/}
+                </main>
+                <Footer/>
+            </div>
+
+        </div>
+    ) : null
 }
 
 export default App
---
+++ capsotoneProject/src/components/index.js
@@ -0,0 +1,6 @@
+// For exporting puprose
+
+import Header from './Header/Header.jsx';
+import Footer from './Footer/Footer.jsx';
+
+export { Header, Footer };
