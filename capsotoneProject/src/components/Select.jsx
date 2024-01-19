2a8b49532106d19b58045f88a906ce29e50c0e1d
jun447 <junaidmumtaz16@gmail.com>
2024-01-19 21:20:08 +0500

    Login signup components done using react hook form

---
+++ capsotoneProject/src/components/Select.jsx
@@ -0,0 +1,35 @@
+import React , {useId} from 'react'
+
+function Select({
+    options,
+    label,
+    className='',
+    ...props
+    }, ref) {
+    const id = useId()
+    return (
+        <div className={'w-full'}>
+            {label && <label htmlFor={id} className={'text-sm font-medium text-gray-700'}>
+                {label}
+            </label>
+            }
+            <select
+                id={id}
+                ref={ref}
+                className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+
+                {...props}
+            >
+                {options?.map((option) => (
+                    <option key={option.value} value={option.value}>
+                        {option.label}
+                    </option>
+                ))}
+            </select>
+
+
+        </div>
+    )
+}
+
+export default React.forwardRef(Select)
